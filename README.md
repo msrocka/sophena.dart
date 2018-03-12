9fb402a01eda4b266ee208be1919ac20a803a988
msrocka <michael.srocka@gmail.com>
2018-03-12 17:24:22 +0100

    added fuel groups

---
+++ README.md
@@ -0,0 +1,13 @@
+# sophena.dart
+`sophena.dart` is a Dart port of the model and core functionalities of 
+[Sophena](https://github.com/GreenDelta/Sophena). It is mainly used to test and
+develop new functionalities for Sophena. (It could be used to port some
+functions of Sophena to a [Flutter](https://flutter.io/) app at some time.)
+
+## Usage
+
+```yaml
+  dependencies:
+    sophena:
+      git: git://github.com/msrocka/sophena.dart.git
+```
--- lib/model.dart
+++ lib/model.dart
@@ -47,7 +47,58 @@ enum ProductType {
 }
 
 class ProductGroup extends BaseDataEntity {
-  
+  ProductType type;
+  int index;
+
+  /// Default usage duration of this product group given in years.
+  int duration;
+
+  /// Default fraction [%] of the investment that is used for repair.
+  double repair;
+
+  /// Default fraction [%] of the investment that is used for maintenance.
+  double maintenance;
+
+  /// Default amount of hours that are used for operation in one year.
+  double operation;
 }
 
-abstract class AbstractProduct extends BaseDataEntity {}
+abstract class AbstractProduct extends BaseDataEntity {
+  double purchasePrice;
+  String url;
+  Manufacturer manufacturer;
+  ProductType type;
+  ProductGroup group;
+}
+
+enum FuelGroup {
+  BIOGAS,
+  NATURAL_GAS,
+  LIQUID_GAS,
+  HEATING_OIL,
+  PELLETS,
+  ELECTRICITY,
+  HOT_WATER,
+  PLANTS_OIL,
+  WOOD
+}
+
+class Fuel extends BaseDataEntity {
+  /// The standard unit of the fuel (e.g. L, m3, kg).
+  String unit;
+
+  /// The calorific value in kWh per 1 standard unit.
+  double calorificValue;
+
+  /// Only for wood fuels: density in kg per solid cubic meter.
+  double density;
+
+  FuelGroup group;
+
+  /// Gramme CO2 emissions per kWh fuel energy.
+  double co2Emissions;
+
+  double primaryEnergyFactor;
+
+  bool isWood() => group == FuelGroup.WOOD;
+}
