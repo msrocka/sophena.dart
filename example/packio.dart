d228ab74e9c5bc62c86c3a2b8271580d2f7ff9f3
msrocka <michael.srocka@gmail.com>
2018-03-13 14:36:08 +0100

    data packs: write content and save them

--- example/packio.dart
+++ example/packio.dart
@@ -1,12 +1,24 @@
 import 'package:sophena/sophena.dart';
+import 'package:logging/logging.dart';
 
 // an example for reading data from a Sophena data package.
 void main() {
-  var path = 'C:/Users//Besitzer/Projects/sophena/data/modelle/Eichstätt_updated.sophena';
+  // setup logging
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((LogRecord rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+
+  var path =
+      'C:/Users//Besitzer/Projects/sophena/data/modelle/Eichstätt_updated.sophena';
   var pack = new DataPack.open(path);
   pack.getIds(ModelType.FUEL).forEach((id) {
     var json = pack.get(ModelType.FUEL, id);
     Fuel fuel = new Fuel.fromJson(json);
     print(fuel.toJson());
   });
+
+  pack.put(ModelType.FUEL, {'id': 'atest'});
+
+  pack.save('C:/Users//Besitzer/Desktop/rems/testpack.sophena');
 }
--- lib/src/datapack.dart
+++ lib/src/datapack.dart
@@ -2,19 +2,27 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:archive/archive.dart';
+import 'package:logging/logging.dart';
 
 import 'enums.dart';
 
 class DataPack {
   Archive _archive;
+  final Logger _log = new Logger('sophena.DataPack');
+
+  DataPack() {
+    _archive = new Archive();
+  }
 
   DataPack.open(String path) {
+    _log.fine('Open data pack $path');
     File file = new File(path);
     List<int> bytes = file.readAsBytesSync();
     _archive = new ZipDecoder().decodeBytes(bytes);
   }
 
   List<String> getIds(ModelType type) {
+    _log.finest('Get IDs for $type');
     String dir = _path(type);
     List<String> ids = [];
     for (ArchiveFile f in _archive.files) {
@@ -36,6 +44,9 @@ class DataPack {
     return _archive.findFile(path) != null;
   }
 
+  /// Loads the json object for the given [type] and [id] from the package.
+  ///
+  /// It simply returns `null` if it does not exist.
   Map<String, dynamic> get(ModelType type, String id) {
     if (type == null || id == null) {
       return null;
@@ -48,45 +59,78 @@ class DataPack {
     return JSON.decode(UTF8.decode(f.content)) as Map<String, dynamic>;
   }
 
-  String _path(ModelType type) {
-    if (type == null) return "unknown";
-    switch (type) {
-      case ModelType.BOILER:
-        return "boilers";
-      case ModelType.BUFFER:
-        return "buffers";
-      case ModelType.BUILDING_STATE:
-        return "building_states";
-      case ModelType.CONSUMER:
-        return "consumers";
-      case ModelType.COST_SETTINGS:
-        return "cost_settings";
-      case ModelType.FLUE_GAS_CLEANING:
-        return "flue_gas_cleaning";
-      case ModelType.FUEL:
-        return "fuels";
-      case ModelType.HEAT_RECOVERY:
-        return "heat_recovery";
-      case ModelType.LOAD_PROFILE:
-        return "load_profiles";
-      case ModelType.MANUFACTURER:
-        return "manufacturers";
-      case ModelType.PIPE:
-        return "pipes";
-      case ModelType.PRODUCER:
-        return "producers";
-      case ModelType.PRODUCT:
-        return "products";
-      case ModelType.PRODUCT_GROUP:
-        return "product_groups";
-      case ModelType.PROJECT:
-        return "projects";
-      case ModelType.TRANSFER_STATION:
-        return "transfer_stations";
-      case ModelType.WEATHER_STATION:
-        return "weather_stations";
-      default:
-        return "unknown";
+  void put(ModelType type, Map<String, dynamic> json) {
+    if (type == null || json == null) {
+      _log.warning('Cannot add json for $type: type or json is null');
+      return;
     }
+    String id = json['id'];
+    if (id == null) {
+      _log.warning('Cannot add json for $type: id is null');
+      return;
+    }
+    String path = '${_path(type)}/$id.json';
+    ArchiveFile f = _archive.findFile(path);
+    if (f != null) {
+      _log.warning('Cannot add json $path as it already exists');
+      return;
+    }
+    List<int> content = UTF8.encode(JSON.encode(json));
+    f = new ArchiveFile(path, content.length, content);
+    _archive.addFile(f);
+  }
+
+  /// Saves the data pack to a file with the given [path].
+  void save(String path) {
+    _log.fine('Write data pack to $path');
+    try {
+      File file = new File(path);
+      List<int> bytes = new ZipEncoder().encode(_archive);
+      file.writeAsBytesSync(bytes);
+    } catch (e) {
+      _log.severe('Failed to save data pack to $path', e);
+    }
+  }
+}
+
+String _path(ModelType type) {
+  if (type == null) return "unknown";
+  switch (type) {
+    case ModelType.BOILER:
+      return "boilers";
+    case ModelType.BUFFER:
+      return "buffers";
+    case ModelType.BUILDING_STATE:
+      return "building_states";
+    case ModelType.CONSUMER:
+      return "consumers";
+    case ModelType.COST_SETTINGS:
+      return "cost_settings";
+    case ModelType.FLUE_GAS_CLEANING:
+      return "flue_gas_cleaning";
+    case ModelType.FUEL:
+      return "fuels";
+    case ModelType.HEAT_RECOVERY:
+      return "heat_recovery";
+    case ModelType.LOAD_PROFILE:
+      return "load_profiles";
+    case ModelType.MANUFACTURER:
+      return "manufacturers";
+    case ModelType.PIPE:
+      return "pipes";
+    case ModelType.PRODUCER:
+      return "producers";
+    case ModelType.PRODUCT:
+      return "products";
+    case ModelType.PRODUCT_GROUP:
+      return "product_groups";
+    case ModelType.PROJECT:
+      return "projects";
+    case ModelType.TRANSFER_STATION:
+      return "transfer_stations";
+    case ModelType.WEATHER_STATION:
+      return "weather_stations";
+    default:
+      return "unknown";
   }
 }
